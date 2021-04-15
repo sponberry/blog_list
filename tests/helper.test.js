341c905dadff6947f394653e8fa5362d0c5aad5b
abigailNottingham <abigailillustration@gmail.com>
2021-04-15 18:23:36 +0100

    completed 4.5, defined tests for totalLikes function

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -4,3 +4,66 @@ test("dummy returns one", () => {
   const result = listHelper.dummy([])
   expect(result).toBe(1)
 })
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  const listWithMoreBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    }
+  ]
+
+  test("when list has more blogs equals the sum of all likes", () => {
+    const result = listHelper.totalLikes(listWithMoreBlogs)
+    expect(result).toBe(34)
+  })
+
+  test("when list is empty equals 0", () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (item, blog) => {
+    return blog.likes + item
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
