64f2cdcab6475b0550c38a5b28c580e0cfa0eed3
sponberry <74988392+sponberry@users.noreply.github.com>
2021-04-15 10:34:14 +0100

    Added logger middleware, toJSON method changes and tested using rest file

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const config = require("./utils/config")
 const logger = require("./utils/logger")
+const middleware = require("./utils/middleware")
 const app = express()
 const blogRouter = require("./controllers/blogs")
 
+logger.info("connecting...")
 
 const mdbParams = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }
 mongoose.connect(config.MONGODB_URI, mdbParams)
@@ -18,6 +20,8 @@ mongoose.connect(config.MONGODB_URI, mdbParams)
 
 app.use(cors())
 app.use(express.json())
+
+app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogRouter)
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,31 @@
 const blogRouter = require("express").Router()
-const Blog = require("./models/blog")
+const Blog = require("../models/blog")
 
 
-blogRouter.get("/", (request, response) => {
+blogRouter.get("/", (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
+    .catch(error => next(error))
 })
 
-blogRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body)
+blogRouter.post("/", (request, response, next) => {
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  })
 
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
+    })
+    .catch(error => {
+      next(error)
     })
 })
 
