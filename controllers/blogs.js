9e84d696433d0a81d82e8e07c19b371003417ec7
abigailNottingham <abigailillustration@gmail.com>
2021-04-22 14:16:13 +0100

    Added functionality to view single blog by id, delete a blog by id, and update blog likes by id to controllers.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,6 +7,11 @@ blogRouter.get("/", async (request, response) => {
   response.json(blogs)
 })
 
+blogRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog)
+})
+
 blogRouter.post("/", async (request, response) => {
   const blog = new Blog({
     title: request.body.title,
@@ -14,9 +19,21 @@ blogRouter.post("/", async (request, response) => {
     url: request.body.url,
     likes: request.body.likes || 0,
   })
-
   const result = await blog.save()
   response.status(201).json(result)
+})
+
+blogRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put("/:id", async (request, response) => {
+  const blog = {
+    likes: request.body.likes
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new:true })
+  response.json(updatedBlog)
 })
 
 module.exports = blogRouter
